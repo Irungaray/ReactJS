7b9a242953bef1c49ebe21ed9aa40b570ee5b019
Irungaray <LionelIrungaray@GMail.com>
2020-05-14 14:46:42 -0300

    Cuarta clase, metiendole garompas

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Button text="Click" />
+    <Button text="Click 2" />
   </React.StrictMode>,
   document.getElementById('root')
 );
